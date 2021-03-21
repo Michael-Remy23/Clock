e15bf8781b2152e87d7ed72d27a3426a10453664
Michael-Remy23 <81110772+Michael-Remy23@users.noreply.github.com>
2021-03-21 15:37:12 -0400

    Add files via upload
    The second hand constantly ticking was the hardest part for me when making this clock but I am very proud of the finished product.

---
+++ time.js
@@ -0,0 +1,17 @@
+const hour_hand = document.querySelector("#hour")
+const minute_hand = document.querySelector("#minute")
+const second_hand = document.querySelector("#second")
+
+
+function time(){
+    var current_time = new Date();
+    var hour = current_time.getHours() *30
+    var minute = current_time.getMinutes() *6
+    var second = current_time.getSeconds() *6
+
+    hour_hand.style.transform = "rotate("+(hour+minute/12)+"deg)";
+    minute_hand.style.transform = "rotate("+minute+"deg)";
+    second_hand.style.transform = "rotate("+second+"deg)";
+}
+setInterval(time, 1000)
+
